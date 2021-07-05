4abee8b0e19d4d29ca8ebbced632272fa582e2fa
thiagovidalpassosgit <thiagovidalpassos@gmail.com>
2021-07-04 21:23:03 -0300

    metodos

--- PersonService/src/main/java/br/com/vidal/controller/PersonController.java
+++ PersonService/src/main/java/br/com/vidal/controller/PersonController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,17 +17,40 @@ import br.com.vidal.services.PersonService;
 @RequestMapping("/person")
 public class PersonController {
 	
-    @Autowired
-    private PersonService services; //com a notation o proprio spring cria a parte "= new PersonServices()	
+	@Autowired
+	private PersonService service;
 	
-	@RequestMapping(method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(method=RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Person> findAll() {
-		return services.findAll();	
-	}
-    
-    @RequestMapping(value="/{id}/", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+		return service.findAll();
+	}	
+	
+	@RequestMapping(value="/{id}",
+			method=RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person findById(@PathVariable("id") String id) {
-		return services.findById(id);	
+		return service.findById(id);
+	}	
+	
+	@RequestMapping(method=RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) {
+		return service.create(person);
 	}
+	
+	@RequestMapping(method=RequestMethod.PUT,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) {
+		return service.update(person);
+	}	
+	
+	@RequestMapping(value="/{id}", 
+			method=RequestMethod.DELETE)
+	public void delete(@PathVariable("id") String id) {
+		service.delete(id);
+	}	
 	
 }
--- PersonService/src/main/java/br/com/vidal/services/PersonService.java
+++ PersonService/src/main/java/br/com/vidal/services/PersonService.java
@@ -14,6 +14,18 @@ public class PersonService {
 	
 	private final AtomicLong counter = new AtomicLong();
 	
+	public Person create(Person person) {
+		return person;
+	}
+	
+	public Person update(Person person) {
+		return person;
+	}	
+	
+	public void delete(String id) {
+		
+	}	
+	
 	public Person findById(String id) {
 		
 		Person person = new Person();
